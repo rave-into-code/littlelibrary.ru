d426b6712fe55474f395507f90f613101eba0da3
Ignat Insarov <kindaro@gmail.com>
2015-07-06 16:05:22 +0300

    Initial state.

---
+++ htdocs/www/404.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<head>
+
+<meta name=keywords content="Bookcrossing в России, bookcrossing in Russia, буккроссинг в России, буккроссинг, bookcrossing, book, книговорот, библиотека, книги, книга, обмен, обмен книгами, всемирный, международный буккроссинг, международный буккроссинг в России"/>
+<meta name=description content="Буккроссинг-портал"/>
+<meta name="robots" content="all"/>
+<title>Международный буккроссинг в России</title>
+
+</head>
+
+
+
+<body bgcolor="#FFFFFF" link="#333333" vlink="#666666" alink="#FFFFCC">
+
+<br><br><center><h2>
+404!!!
+</h2>
+Дорогие друзья!<br><br>
+Произошло досадное недоразумение или это может быть даже злой умысел <br>
+врагов человечества, <br>
+но в итоге страницы этой уже нет и не известно, куда она пропала и вообще,<br> 
+была ли она.
+
+
+<br><br><br>
+<a href="http://www.ballycumber.ru/">Перейти на наш сайт хоть как-нибудь</a>
+<br><br><br>
+<br>
+<a href="http://www.bookcrossing.com/"><img src="down/516769__.jpg"></a>
+<br><br>
+
+<a href="http://www.bookcrossing.com/"><img src="down/bookcrossing14.jpg"></a>
+<br><br>
+</center>
+</body>
+</html>
